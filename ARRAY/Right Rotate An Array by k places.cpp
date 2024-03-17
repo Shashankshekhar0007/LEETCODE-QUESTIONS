3fad879f848a5522f293a7fd32ef5971a859078c
Shashankshekhar0007 <shashankshekhar564@gmail.com>
2024-03-18 01:22:35 +0530

    Array Problems

---
+++ ARRAY/Right Rotate An Array by k places.cpp
@@ -0,0 +1,11 @@
+void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        vector<int> temp(n,0);
+        for(int i=0;i<n;i++){
+            temp[(i+k)%n]=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=temp[i];
+        }
+    }
