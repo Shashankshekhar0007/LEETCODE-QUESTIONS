3c160cd31e4000c4ac4f90d31349269ce85e257b
Shashankshekhar0007 <shashankshekhar564@gmail.com>
2024-03-28 13:00:11 +0530

    flatten tree using stack

---
+++ BINARY TREE/Flattening a Tree using a stack.cpp
@@ -0,0 +1,21 @@
+class Solution{
+  public:
+         void flatten(TreeNode*root){
+          stack<TreeNode*>st;
+          st.push(root);
+          while(!st.empty()){
+            TreeNode* curr= st.top();
+            st.pop();
+            if(curr->right){
+              st.push(curr->right);
+            }
+            if(curr->left){
+              st.push(curr->left);
+            }
+            if(!st.empty()){
+              curr->right=st.top();
+              curr->left=null;
+            }
+          }
+         }
+};
